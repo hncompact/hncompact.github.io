37c000790d76dbe03c167f0470c3dcddff82598c
hncompact <hncompact@ml1.net>
2025-02-03 13:47:36 -0800

    Update hncompact.user.js

--- hncompact.user.js
+++ hncompact.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name        HN SuperCompact
+// @name        HN SuperCompact UI
 // @match       https://news.ycombinator.com/*
 // @grant       none
-// @version     1.2
+// @version     1.3
 // @author      hncompact
 // @description Makes the HN UI even more compact than it is now.
 // @license     MIT
@@ -12,12 +12,13 @@
 let POSTS_CSS = `
   <style>
   .votelinks a { display:none }
+  tr.submission td { font-size:8pt }
   tr.submission .votelinks center { min-width:1em }
   tr.submission.recent .votelinks center::before { content:'*'; color:#6f0; }
   tr.submission + tr { display:none }
-  tr.submission td:not(:last-child) { line-height:2em; font-size:8pt; white-space:nowrap; text-align:right; vertical-align:top; }
-  tr.submission td:first-child a { text-decoration:underline }
-  tr.submission .titleline .sitebit { display:none }
+  tr.submission td:not(:last-child) { white-space:nowrap; text-align:right; vertical-align:top; }
+  tr.submission td:first-child a:hover { text-decoration:underline }
+  tr.submission .titleline .sitebit {  }
   </style>
 `;
 
@@ -26,9 +27,11 @@ let COMMENTS_CSS = `
   tr.comtr .votelinks center { min-width:1em }
   tr.comtr .votelinks a { display:none }
   tr.comtr:not(.coll) .reply { font-size:8pt; opacity:0.5; }
-  td.ind[indent="0"] + .votelinks + .default .commtext::first-letter { text-decoration:underline }
+  td.ind[indent="0"] + .votelinks center::before { content:'*'; color:#444; }
   tr.comtr.recent .votelinks center::before { content:'*'; color:#6f0 !important; }
   tr.comtr .default > *:not(.comment) { display:none }
+  tr.comtr a.hnuser { text-decoration:none }
+  tr.comtr .navs *:not(.togg) { display:none }
   tr.comtr.coll .comment.noshow { display:inherit }
   tr.comtr.coll .comment.noshow .commtext { display:none }
   tr.comtr.coll .comment.noshow .reply > p > *:not(.comhead) { display:none }
@@ -65,20 +68,24 @@ function initPosts() {
     let subtext = tr2.textContent.trim();
     let [,comms='0'] = subtext.match(/(\d+)\scomments/)||[];
     let [,score='0'] = subtext.match(/(\d+)\spoints/)||[];
-    let [,time='0', timeUnits=''] = subtext.match(/(\d+)\s(\w+)\sago/)||[];
+    let [,time='0', units=''] = subtext.match(/(\d+)\s(\w+)\sago/)||[];
     let td1 = tr.children[0];
     td1.className = '';
     td1.textContent = '';
 
+    let bcomms = document.createElement('b');
+    bcomms.textContent = comms;
+
     let link = tr2.querySelector('a:last-child');
     link.remove();
-    link.textContent = comms;
-    td1.append('+' + score + ':', link, '-' + time + (timeUnits[0]||''));
+    link.textContent = '';
+    link.append('+' + score + ':', bcomms, '-' + time, units[0]||'');
+    td1.append(link);
 
     for (let a of tr.querySelectorAll('.votelinks a'))
       a.remove();
 
-    if (timeUnits.match(/minute/))
+    if (units.match(/minute/))
       tr.classList.add('recent');
   }
 }
@@ -103,8 +110,21 @@ function initComments() {
     comhead.remove();
     reply.append(comhead);
 
-    let age = reply.querySelector('.age');
-    if (age && age.textContent.match(/minute/))
-      tr.classList.add('recent');
+    let age = reply.querySelector('.age a');
+    if (age) {
+      let [time, units=''] = age.textContent.split(' ');
+      age.textContent = '-' + time + units[0];
+      if (units.match(/minute/))
+        tr.classList.add('recent');
+    }
+
+    let navs = comhead.querySelector('.navs');
+    for (let node of navs ? navs.childNodes : [])
+      if (node.nodeName == '#text')
+        node.remove();
+
+    let collapse = navs && navs.querySelector('a.togg');
+    if (collapse && collapse.textContent.match(/^\[.\]$/))
+      collapse.textContent = '[collapse]';
   }
 }
